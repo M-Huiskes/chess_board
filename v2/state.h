3b9d75f7a32d331d161c0449678f98aca3ed84c4
Merijn <merijn-huiskes@outlook.com>
2026-05-03 10:07:34 +0200

    feat: Added move_history struct

--- v2/state.h
+++ v2/state.h
@@ -3,11 +3,37 @@
 
 #include "board.h"
 
+#include <stdint.h>
+
+typedef enum {
+    QUIET = 0,
+    DOUBLE_PAWN = 1,
+    CASTLE_KING = 2,
+    CASTLE_QUEEN = 3,
+    CAPTURE = 4,
+    EN_PASSANT = 5,
+    PROMO_KNIGHT = 8,
+    PROMO_BISHOP = 9,
+    PROMO_ROOK = 10,
+    PROMO_QUEEN = 11,
+    PROMO_CAPTURE_KNIGHT = 12,
+    PROMO_CAPTURE_BISHOP = 13,
+    PROMO_CAPTURE_ROOK = 14,
+    PROMO_CAPTURE_QUEEN = 15,
+} MoveFlag;
+
+typedef struct {
+    uint16_t *moves;
+    int count;
+    int capacity;
+} MoveHistory;
+
 typedef struct GameState {
     BoardState board;
     Piece *selected_piece;
     Square selected_square;
     int bit_position;
+    MoveHistory move_history;
 } GameState;
 
 GameState init_game_state();
