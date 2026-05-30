094f4882262c2ba0e2ba6d0b760710628952bff8
Merijn <merijn-huiskes@outlook.com>
2026-05-30 21:22:59 +0200

    feat: Fixed promotion to knight (presumably, kinda hard to test)

--- v2/computer.c
+++ v2/computer.c
@@ -48,79 +48,117 @@ static int eval_moves(GameState *game_state, int piece_start, int piece_end,
                 if (piece->color == 'w'
                         ? output_square.row == 7 && piece->symbol == 'P'
                         : output_square.row == 0 && piece->symbol == 'p') {
-                    // char color_playing = game_state->selected_piece->color;
-                    // char pieces[2];
-                    // if (color_playing == 'w') {
-                    //     pieces[0] = 'Q';
-                    //     pieces[1] = 'N';
-                    // } else {
-                    //     pieces[0] = 'q';
-                    //     pieces[1] = 'n';
-                    // }
-                    // for (int i = 0; i < 2; i++) {
-                    //     game_state->promote_to = pieces[i];
-                    //     make_move(game_state);
-
-                    //     // TODO: Fix this, this is very ugly, just to check
-                    //     // whether it now works correctly!!!
-                    //     game_state->selected_square =
-                    //         square_from_position(position);
-                    //     game_state->bit_position = position;
-                    //     game_state->selected_piece = piece;
-
-                    //     int score = minimax(game_state, depth - 1,
-                    //     !is_maxing);
-
-                    //     if (is_maxing ? score > best_score
-                    //                   : score < best_score) {
-                    //         best_score = score;
-                    //         game_state->computer_move = (ComputerMove){
-                    //             .from = game_state->bit_position,
-                    //             .to = game_state->output_position,
-                    //             .piece = game_state->selected_piece->symbol,
-                    //         };
-                    //         printf("Computer move, piece %c from %d to %d
-                    //         with "
-                    //                "score %d\n",
-                    //                game_state->computer_move.piece,
-                    //                game_state->computer_move.from,
-                    //                game_state->computer_move.to, score);
-                    //     }
-                    //     unmake_move(game_state, old_state);
-                    // }
-                    promote_to = piece->color == 'w' ? 'Q' : 'q';
-                    game_state->promote_to = promote_to;
-                }
-                make_move(game_state);
-
-                // TODO: Fix this, this is very ugly, just to check whether
-                // it now works correctly!!!
-                game_state->selected_square = square_from_position(position);
-                game_state->bit_position = position;
-                game_state->selected_piece = piece;
-
-                int score = minimax(game_state, depth - 1, !is_maxing);
-
-                if (is_maxing ? score > best_score : score < best_score) {
-                    best_score = score;
-                    game_state->computer_move = (ComputerMove){
-                        .from = game_state->bit_position,
-                        .to = game_state->output_position,
-                        .piece = game_state->selected_piece->symbol,
-                        .promote_to = '0',
-                    };
-                    printf("Gamestate promote to %c\n", promote_to);
-                    if (promote_to != '0') {
-                        game_state->computer_move.promote_to = promote_to;
+                    char color_playing = game_state->selected_piece->color;
+                    char pieces[2];
+                    if (color_playing == 'w') {
+                        pieces[0] = 'Q';
+                        pieces[1] = 'N';
+                    } else {
+                        pieces[0] = 'q';
+                        pieces[1] = 'n';
                     }
-                    printf("Computer move, piece %c from %d to %d with "
-                           "score %d\n",
-                           game_state->computer_move.piece,
-                           game_state->computer_move.from,
-                           game_state->computer_move.to, score);
+                    for (int i = 0; i < 2; i++) {
+                        promote_to = pieces[i];
+                        game_state->promote_to = promote_to;
+                        make_move(game_state);
+
+                        // TODO: Fix this, this is very ugly, just to check
+                        // whether it now works correctly!!!
+                        game_state->selected_square =
+                            square_from_position(position);
+                        game_state->bit_position = position;
+                        game_state->selected_piece = piece;
+
+                        int score = minimax(game_state, depth - 1, !is_maxing);
+
+                        if (is_maxing ? score > best_score
+                                      : score < best_score) {
+                            best_score = score;
+                            game_state->computer_move = (ComputerMove){
+                                .from = game_state->bit_position,
+                                .to = game_state->output_position,
+                                .piece = game_state->selected_piece->symbol,
+                                .promote_to = '0'};
+
+                            if (promote_to != '0') {
+                                game_state->computer_move.promote_to =
+                                    promote_to;
+                            }
+                            printf("Computer move, piece %c from %d to %d with "
+                                   "score %d\n",
+                                   game_state->computer_move.piece,
+                                   game_state->computer_move.from,
+                                   game_state->computer_move.to, score);
+                        }
+                        unmake_move(game_state, old_state);
+                        promote_to = '0';
+                    }
+                    promote_to = piece->color == 'w' ? 'Q' : 'q';
+                    game_state->promote_to = piece->color == 'w' ? 'Q' : 'q';
+
+                    make_move(game_state);
+
+                    // TODO: Fix this, this is very ugly, just to check whether
+                    // it now works correctly!!!
+                    game_state->selected_square =
+                        square_from_position(position);
+                    game_state->bit_position = position;
+                    game_state->selected_piece = piece;
+
+                    int score = minimax(game_state, depth - 1, !is_maxing);
+
+                    if (is_maxing ? score > best_score : score < best_score) {
+                        best_score = score;
+                        game_state->computer_move = (ComputerMove){
+                            .from = game_state->bit_position,
+                            .to = game_state->output_position,
+                            .piece = game_state->selected_piece->symbol,
+                            .promote_to = '0',
+                        };
+                        printf("Gamestate promote to %c\n", promote_to);
+                        if (promote_to != '0') {
+                            game_state->computer_move.promote_to = promote_to;
+                        }
+                        printf("Computer move, piece %c from %d to %d with "
+                               "score %d\n",
+                               game_state->computer_move.piece,
+                               game_state->computer_move.from,
+                               game_state->computer_move.to, score);
+                    }
+                    unmake_move(game_state, old_state);
+                    promote_to = '0';
+                } else {
+                    make_move(game_state);
+
+                    // TODO: Fix this, this is very ugly, just to check whether
+                    // it now works correctly!!!
+                    game_state->selected_square =
+                        square_from_position(position);
+                    game_state->bit_position = position;
+                    game_state->selected_piece = piece;
+
+                    int score = minimax(game_state, depth - 1, !is_maxing);
+
+                    if (is_maxing ? score > best_score : score < best_score) {
+                        best_score = score;
+                        game_state->computer_move = (ComputerMove){
+                            .from = game_state->bit_position,
+                            .to = game_state->output_position,
+                            .piece = game_state->selected_piece->symbol,
+                            .promote_to = '0',
+                        };
+                        if (promote_to != '0') {
+                            game_state->computer_move.promote_to = promote_to;
+                        }
+                        printf("Computer move, piece %c from %d to %d with "
+                               "score %d\n",
+                               game_state->computer_move.piece,
+                               game_state->computer_move.from,
+                               game_state->computer_move.to, score);
+                    }
+                    unmake_move(game_state, old_state);
+                    promote_to = '0';
                 }
-                unmake_move(game_state, old_state);
-                promote_to = '0';
                 possible_moves &= possible_moves - 1;
             }
             piece_bb &= piece_bb - 1;
